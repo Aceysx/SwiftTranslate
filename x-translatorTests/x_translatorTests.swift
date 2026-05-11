4c52c065d45bec94d9360a5b7ad5bb2268556005
xin <Aceysx@users.noreply.github.com>
2026-05-11 13:42:58 +0800

    Initial Commit

---
+++ x-translatorTests/x_translatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  x_translatorTests.swift
+//  x-translatorTests
+//
+//  Created by xin si on 2026/5/11.
+//
+
+import Testing
+@testable import x_translator
+
+struct x_translatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
